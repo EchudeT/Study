fa56d65d16bdba3fac705b48aebd0b100c8206f7
EchudeT <152363474+EchudeT@users.noreply.github.com>
2024-01-06 11:57:40 +0800

    Add files via upload

---
+++ simple_server_and_client/c#/Client/Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Core;
+using Count;
+
+var channel = new Grpc.Core.Channel("localhost:5001", ChannelCredentials.Insecure);
+var client = new CountNum.CountNumClient(channel);
+
+var response = client.QuickCount(new CountRequest { Numa = 1,Numb = 2, Opt = 3 });
+
+Console.WriteLine(response.ReNum);
